e4c8c288bbccb54f4f2355d62ed1f2ccba622c2f
Ringinalx <72195083+Ringinalx@users.noreply.github.com>
2022-03-28 15:46:29 +0100

    Add files via upload

---
+++ DesignPrinciplesLab/singleOpenClosed/src/main/java/ie/gmit/open/Subscriber.java
@@ -0,0 +1,61 @@
+package ie.gmit.open;
+
+public abstract class Subscriber {
+
+   protected Long subscriberId;
+
+    protected String address;
+
+    protected Long phoneNumber;
+
+
+
+
+    /**
+     * @return the subscriberId
+     */
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    /**
+     * @param subscriberId the subscriberId to set
+     */
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+public abstract double calculateBill(); //extension
+
+
+}
